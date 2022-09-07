2e17ba155a372cfbbfe7ae4bee090d17d68ab753
junior516 <someguycalledjunior516@gmail.com>
2022-09-07 12:57:52 +0000

    removed file

--- CubeLab.java
+++ CubeLab.java
@@ -1,16 +1,19 @@
 import java.util.*;
+
 class Program {
-static int getSurfaceAreaFromEdgeLength(int edgelength) // function to get surface area from edge length 
+      static int getSurfaceAreaFromEdgeLength(int edgelength) // function to get surface area from edge length
       {
-      int totalEdgeLength = 6*(edgelength*edgelength);
-      return totalEdgeLength;
+            int totalEdgeLength = 6 * (edgelength * edgelength);
+            return totalEdgeLength;
       }
-   public static void main(String args[]) {
-      Scanner myScanner = new Scanner(System.in);
-      System.out.println("Type in the length of an edge to get the surface area of the cube in units:");
-      int elength = myScanner.nextInt(); // reads next line for integer
-      int surfacearea = getSurfaceAreaFromEdgeLength(elength); // retrieve surface area from edge length
-      System.out.println("Edge length is: " + elength + " u"); // Outputs edge length
-      System.out.println("Surface area is: " + surfacearea + " u^2"); // Outputs surface area
-}
+
+      public static void main(String args[]) {
+            Scanner myScanner = new Scanner(System.in);
+            System.out.println("Type in the length of an edge to get the surface area of the cube in units:");
+            int elength = myScanner.nextInt(); // reads next line for integer
+            int surfacearea = getSurfaceAreaFromEdgeLength(elength); // retrieve surface area from edge length
+            System.out.println("Edge length is: " + elength + " u"); // Outputs edge length
+            System.out.println("Surface area is: " + surfacearea + " u^2"); // Outputs surface area
+            myScanner.close();
+      }
 }
--- SphereLab.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class SphereLab {
-   public static void main(String[] args) {
-     Scanner massScanner = new Scanner(System.in);
-     Scanner velocityScanner = new Scanner(System.in);
-     
-   }
-}
