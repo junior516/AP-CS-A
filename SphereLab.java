e3adfb1ca48c70b9d8e245c7449e8e2256d4bc81
junior516 <113032975+junior516@users.noreply.github.com>
2022-09-07 08:52:07 -0400

    Add files via upload

---
+++ SphereLab.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class SphereLab {
+   public static void main(String[] args) {
+     Scanner massScanner = new Scanner(System.in);
+     Scanner velocityScanner = new Scanner(System.in);
+     
+   }
+}
