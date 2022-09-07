7a8a4635c4e1c13240a339628c26a7cf8a8bb6fc
junior516 <someguycalledjunior516@gmail.com>
2022-09-07 23:17:55 +0000

    my kinetic energy lab fun

--- KineticEnergyLab.java
+++ KineticEnergyLab.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class KineticEnergyLab {
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        // retrieve mass
+        System.out.println("Enter mass here in kg: ");
+        int mass = myScanner.nextInt();
 
+        // retrieve velocity
+        System.out.println("Enter velocity here in m/s: ");
+        int velocity = myScanner.nextInt();
+        // output momentum
+        System.out.println("Momentum: " + (mass*velocity) + " kg m/s");
+
+        // output kinetic energy
+        System.out.println("Kinetic Energy: " + 0.5*(mass*Math.pow(velocity, 2)) + " J");
+        myScanner.close();
     }
 }
