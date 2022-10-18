e96b4046b230b5d9ef95321c37e861eb73da514d
junior516 <someguycalledjunior516@gmail.com>
2022-10-18 12:50:02 +0000

    lol lets go

---
+++ RandoString/QuotientAndRemainder.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class QuotientAndRemainder {
+    public static void main(String[] args) {
+        // scanner to retrieve data
+        Scanner input = new Scanner(System.in);
+        // retrieve dividend
+        System.out.println("Choose the dividend: ");
+        int dividend = input.nextInt();
+        // retrieve divisor
+        System.out.println("Choose a divisor: ");
+        int divisor = input.nextInt();
+        int quotient; 
+        int remainder; 
+        // retrieve remainder and quotient
+        remainder = dividend % divisor;
+        quotient = dividend / divisor;
+        // output answer
+        System.out.println(quotient + "r" + remainder);
+        input.close();
+    }
+}
