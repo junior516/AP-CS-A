d7b584bdf3bf0c43bf2d7fd8077e11c25c7e2d38
junior516 <someguycalledjunior516@gmail.com>
2022-12-03 03:21:08 +0000

    asdasdadqwdqdqdqdww

---
+++ RandoString/Die.java
@@ -0,0 +1,16 @@
+// Die Class
+public class Die {
+    private int side = 1;
+
+    public Die() {
+        roll();
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void roll() {
+        side = (int) (Math.random() * 6) + 1;
+    }
+}
