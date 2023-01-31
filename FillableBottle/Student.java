705a7ec333f97c75dda688c05dbaf4d5a4d066db
junior516 <someguycalledjunior516@gmail.com>
2023-01-31 22:27:08 +0000

    LUBAIBAEJPGJQP KJOPQKFPOQKEP GOWEKG PKQEGPKPGMPQMGPRNGPHQWRPGWRPGWRGPWRKG

---
+++ FillableBottle/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+   private String name;
+   private int score;
+   public Student(String newName) {
+    name = newName;
+   }
+   public String getName() {
+    return name;
+   } 
+   public int getScore() {
+    return score;
+   } 
+}
